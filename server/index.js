0e6a90dc30e42a7ecf1be9ffaaff88577d0b5570
Furkan Shakib <furkanshakib@gmail.com>
2025-12-31 16:31:13 +0600

    Increased payload limit to 50mb

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const Experience = require('./models/Experience');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Connect to MongoDB (Updated for modern Mongoose)
 mongoose.connect(process.env.MONGO_URI)
