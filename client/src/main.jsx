5dfa21ece714e05099fe8844709a252eb439b2bb
Furkan Shakib <furkanshakib@gmail.com>
2026-01-03 00:23:52 +0600

    Added BrowserRouter to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 👈 IMPORTANT IMPORT
 import App from './App.jsx'
+import { ThemeProvider } from './ThemeContext' // 👈 Keep your theme working
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* 1. The Router wraps everything */}
+      <ThemeProvider> {/* 2. The Theme wraps the App */}
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
